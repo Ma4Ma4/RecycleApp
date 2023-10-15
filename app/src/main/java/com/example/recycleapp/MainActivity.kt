87c2c84b65dea422b4c00e5d6d073bf7fcc3faf7
Ma4Ma4 <aqua.pikamagi@gmail.com>
2023-10-16 03:23:55 +0900

    Initial commit KENSHIN

---
+++ app/src/main/java/com/example/recycleapp/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.recycleapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.CalendarView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // CalendarViewに現在日時を設定します。
+        val calendarView = findViewById<CalendarView>(R.id.calendarView)
+        calendarView.date = System.currentTimeMillis()
+
+        // CalendarViewで日にちが選択された時に呼び出されるリスナー
+        val listener = DateChangeListener()
+        calendarView.setOnDateChangeListener(listener)
+    }
+
+    // CalendarViewで日にちが選択された時に呼び出されるリスナークラス
+    private inner class DateChangeListener : CalendarView.OnDateChangeListener {
+        override fun onSelectedDayChange(calendarView: CalendarView, year: Int, month: Int, dayOfMonth: Int) {
+            // monthは0起算のため+1します。
+            val displayMonth = month + 1
+            Toast.makeText(applicationContext, "$year/$displayMonth/$dayOfMonth", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
