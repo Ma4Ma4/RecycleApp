d6560bf7d98531b09b5cd5e8ada1ebee7336b0e0
Ma4Ma4 <aqua.pikamagi@gmail.com>
2023-10-16 16:48:38 +0900

    [カン]でテキスト切り替え版

--- app/src/main/java/com/example/recycleapp/MainActivity.kt
+++ app/src/main/java/com/example/recycleapp/MainActivity.kt
@@ -4,6 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.CalendarView
 import android.widget.Toast
+import android.util.Log
+import android.view.MotionEvent
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,6 +23,17 @@ class MainActivity : AppCompatActivity() {
         // CalendarViewで日にちが選択された時に呼び出されるリスナー
         val listener = DateChangeListener()
         calendarView.setOnDateChangeListener(listener)
+
+        // ボタンクリックを検出するイベントリスナー(カン)
+        val buttonCan:Button = findViewById(R.id.button_can)
+        buttonCan.setOnClickListener {
+            // ここに[カン]ボタン押下時の処理を記述
+            Log.d("INFO","BUTTON CAN WAS PRESSED")
+            val buttonText:TextView = findViewById(R.id.text_by_button)  // ボタンに連動するテキスト
+            buttonText.text = getString(R.string.txt_by_bt_clicked)
+            // カレンダー制御系
+
+        }
     }
 
     // CalendarViewで日にちが選択された時に呼び出されるリスナークラス
