dd47c2819f4aae4bebb06aad2e7c4c41b8dcf362
Ma4Ma4 <aqua.pikamagi@gmail.com>
2023-10-26 08:16:40 +0900

    第1スプリント完了+通知実装版

--- app/src/main/java/com/example/recycleapp/MainActivity.kt
+++ app/src/main/java/com/example/recycleapp/MainActivity.kt
@@ -1,15 +1,22 @@
 package com.example.recycleapp
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import androidx.core.app.NotificationCompat //怪しい,コイツ周りのためにbuildをandroidXへ変更
+import androidx.core.app.NotificationManagerCompat
 import android.util.Log
 import android.view.View
-import android.view.ViewGroup
 import android.widget.Button
 import android.widget.CalendarView
 import android.widget.TextView
 import android.view.ViewGroup.MarginLayoutParams;
-import android.view.ViewGroup.LayoutParams;
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat.EXTRA_NOTIFICATION_ID
 import java.util.*
 
 
@@ -19,6 +26,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         //テストです
+        /*----- 各種ビューの作成・登録-----*/
         // CalendarViewに現在日時を設定します。
         val calendarView = findViewById<CalendarView>(R.id.calendarView)
         calendarView.date = System.currentTimeMillis()
@@ -45,13 +53,79 @@ class MainActivity : AppCompatActivity() {
         // ボタンクリックを検出するイベントリスナー(ビン)
         val buttonBin:Button = findViewById(R.id.button_bin)
         buttonBin.setOnClickListener {
-            // ここに[カン]ボタン押下時の処理を記述
+            // ここに[ビン]ボタン押下時の処理を記述
             Log.d("INFO","BUTTON BIN WAS PRESSED")
             buttonText.text = getString(R.string.txt_bin)
             // カレンダー制御
             highlightLine.setVisibility(View.VISIBLE)
             mlp.setMargins(765,190,0,0)         //マージンを設定
             highlightLine.layoutParams = mlp
+        }
+
+        /*----- 通知周り-----*/
+        // 1-1. チャネルIDを作成
+        val channelId = "NOTIFICATION_CHANNEL_ID"
+
+        // チャネル
+        //2-1. NotificationChannelオブジェクトの作成
+        val name = getString(R.string.channel_name)
+        val descriptionText = getString(R.string.channel_description)
+        val importance = NotificationManager.IMPORTANCE_DEFAULT
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(channelId, name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system
+            // 2-2. チャネルをシステムに登録
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        // 3-1. アプリ内アクティビティへのインテントを設定
+        val intent = Intent(this, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            putExtra(EXTRA_NOTIFICATION_ID, 0)
+        }
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
+
+        // EX. 通知に2択ボタンを設けてアクティビティ遷移の土台にする
+        // ここを遷移先のアクティビティを指定するインテントにすることで実装できそう
+        val intentForgot = Intent(this, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            putExtra(EXTRA_NOTIFICATION_ID, 0)
+        }
+        val pendingIntentForgot = PendingIntent.getActivity(this, 0, intentForgot, 0)
+
+        val builder = NotificationCompat.Builder(this, channelId)
+            // 1-2. 表示内容の設定
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle("テスト通知")
+            .setContentText("水曜日はカンの回収日です。ゴミ出しは完了しましたか?")
+            .setStyle(NotificationCompat.BigTextStyle() //テキスト領域の拡大
+                .bigText("水曜日はカンの回収日です。ゴミ出しは完了しましたか?"))
+            // 1-3. 優先度の設定
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            // 3-2. インテントをbuilderに設定
+            .setContentIntent(pendingIntent) //通知をタップしたらインテントを読み込む
+            .addAction( //ゴミ出しできた選択肢
+                com.google.android.material.R.drawable.ic_clear_black_24, getString(R.string.submit), //アイコンは適当
+            pendingIntent)
+            .addAction( //ゴミ出し忘れた選択肢
+                com.google.android.material.R.drawable.ic_clear_black_24, getString(R.string.forgot), //アイコンは適当
+                pendingIntentForgot)
+
+        //.setAutoCancel(true) //通知をタップしたら通知が消去される
+
+        // ボタンクリックを検出するイベントリスナー(通知トリガー用)
+        val buttonNotify:Button = findViewById(R.id.button_notify)
+        buttonNotify.setOnClickListener {
+            // ここに[通知を発令]ボタン押下時の処理を記述
+            Log.d("INFO","BUTTON NOTIFY WAS PRESSED")
+            with(NotificationManagerCompat.from(this)) {
+                // notificationIDとbuilder.build()を渡します
+                notify(12345, builder.build())
+            }
         }
     }
 
