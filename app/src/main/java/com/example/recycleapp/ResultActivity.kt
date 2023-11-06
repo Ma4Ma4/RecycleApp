b2c9f26d1c9b941f221c27ef9e7bd23d6575c9f0
kazu_ <aggai8931@docomo.ne.jp>
2023-11-06 15:16:27 +0900

    test

--- app/src/main/java/com/example/recycleapp/ResultActivity.kt
+++ app/src/main/java/com/example/recycleapp/ResultActivity.kt
@@ -1,11 +1,25 @@
 package com.example.recycleapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
+
+
 
 class ResultActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_result)
-    }
-}
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_result)
+            val array = arrayOf("1", "2", "3", "4", "5", "6", "7", "8", "9", "10")
+            val listView = findViewById<ListView>(R.id.list_View)
+            val adapter = ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,array)
+            listView.adapter = adapter
+
+            }
+
+        }
+
+
+
+
